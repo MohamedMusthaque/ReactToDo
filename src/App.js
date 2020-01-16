34ef8b7eec1a1e557d5a65dd3d83d6a2921be33e
Mohamed Musthaque <9990475+MohamedMusthaque@users.noreply.github.com>
2020-01-16 23:30:30 +0530

    Local storage
    local storage completed.!

--- src/App.js
+++ src/App.js
@@ -6,18 +6,13 @@ class App extends Component {
   state = {
     //Array of sample todos
     todos: [
-      //{ edit:false, id:1, title:"Buy Vegitables", description:"Vegitables for this week", created_date:"12-01-2020" , deleted:false },
+      { edit:false, id:1, title:"Buy Vegitables", description:"Vegitables for this week", created_date:"12-01-2020" , deleted:false },
       { edit:false, id:2, title:"Shopping", description:"Monthly home goods", created_date:"14-01-2020", deleted:false }
     ]
   }
 
-  componentWillMount(){
-    
-    //console.log(this.state.todos)
-  }
-
   componentDidMount(){
-    //this.getTodo();
+    this.getTodo();
   }
 
   //Delete todo
@@ -30,6 +25,9 @@ class App extends Component {
     this.setState({
       todos //todos: todos - no need to use since key and value having the same name
     });
+
+    //Save to local storage
+    localStorage.setItem("Todos",JSON.stringify(todos))
   }
 
   //Add todo
@@ -50,27 +48,23 @@ class App extends Component {
         let todos = [...this.state.todos, todo];
         this.setState({
           todos
-        });
+        }); 
         
-  }
-
-  //Save todo items to local storage
-  saveTodo = (todo) => {
-    var str = JSON.stringify(todo);
-    localStorage.setItem("todoListItems", str);
+        //Save to local storage
+        localStorage.setItem("Todos",JSON.stringify(todos))
   }
 
   //Get data from local storage
   getTodo = () => {
-    var str = localStorage.getItem("todoListItems");
-    this.setState({
-      todos : JSON.parse(str)
-    });
-    
-    if(!this.state.todos){
+    try {
+      const todoos = localStorage.getItem("Todos");
+      if( todoos === null) return undefined;
       this.setState({
-        todos : []
+        todos : JSON.parse(todoos)
       });
+    }catch(e){
+      console.log(e);
+      return undefined;
     }
   }
 
@@ -107,7 +101,7 @@ class App extends Component {
     return (
       <div className="todo-app container">
         <h1 className="center blue-text">Todo's</h1>
-        <CreateTodo addTodo={this.addTodo}/>
+        <CreateTodo addTodo={this.addTodo} saveTodo={this.saveTodo}/>
         <Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />
       </div>
     );
