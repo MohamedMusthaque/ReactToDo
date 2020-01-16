ccaca4de6fdeeaa8ca33f2862ed4bf274297f71f
Mohamed Musthaque <9990475+MohamedMusthaque@users.noreply.github.com>
2020-01-16 22:45:27 +0530

    duplicate
    check duplicate

--- src/App.js
+++ src/App.js
@@ -6,9 +6,18 @@ class App extends Component {
   state = {
     //Array of sample todos
     todos: [
-      { edit:false, id:1, title:"Buy Vegitables", description:"Vegitables for this week", created_date:"12-01-2020" , deleted:false },
+      //{ edit:false, id:1, title:"Buy Vegitables", description:"Vegitables for this week", created_date:"12-01-2020" , deleted:false },
       { edit:false, id:2, title:"Shopping", description:"Monthly home goods", created_date:"14-01-2020", deleted:false }
     ]
+  }
+
+  componentWillMount(){
+    
+    //console.log(this.state.todos)
+  }
+
+  componentDidMount(){
+    //this.getTodo();
   }
 
   //Delete todo
@@ -25,23 +34,74 @@ class App extends Component {
 
   //Add todo
   addTodo = (todo) => {
+    
+    // this.state.todos.forEach(element => {
+    //   if (element.title === todo['title']){  
+    //     alert( "Title cannot be duplicate.!" );
+    //     return;
+    //   }
+    //   else {
+        
+    //     return;
+    //   }
+    // })
 
     todo.id = Math.random();
-    let todos = [...this.state.todos, todo];
-    this.setState({
-      todos
-    });
+        let todos = [...this.state.todos, todo];
+        this.setState({
+          todos
+        });
+        
+  }
 
-    //Save to local storage
-    localStorage.setItem("Todos",JSON.stringify(todos))
+  //Save todo items to local storage
+  saveTodo = (todo) => {
+    var str = JSON.stringify(todo);
+    localStorage.setItem("todoListItems", str);
+  }
+
+  //Get data from local storage
+  getTodo = () => {
+    var str = localStorage.getItem("todoListItems");
+    this.setState({
+      todos : JSON.parse(str)
+    });
+    
+    if(!this.state.todos){
+      this.setState({
+        todos : []
+      });
+    }
   }
 
   //Edit todo
-  editTodo = (todo) => {
+  editTodo = (title) => {
+    console.log(title); //selected item's id
+
+    const todos = this.state.todos.filter(todo => {
+      if(todo.title !== title){
+        alert("duplicate");
+      }
+      return todo.title 
+    });
     this.setState({
-      edit:true
+      todos
     });
   }
+
+  // fEdit = (i) => {
+  //   let data = this.state.datas[i];
+  //   this.refs.name.value = data.name;
+  //   this.refs.address.value = data.address;
+
+  //   this.setState({
+  //     act: 1,
+  //     index: i
+  //   });
+
+  //   this.refs.name.focus();
+  // }  
+
 
   render() {
     return (
