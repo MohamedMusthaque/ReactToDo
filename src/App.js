aa7905a1b1af7f5d68a021ad7e7a3009fbe6191e
Mohamed Musthaque <mohamedmusthaque@Mohameds-MacBook-Pro.local>
2020-01-16 19:04:22 +0530

    Create Read Delete functions completed

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Todos from './components/Todos';
+import CreateTodo from './components/CreateTodo'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    //Array of sample todos
+    todos: [
+      {edit:false, id:1, title:"Buy Vegitables", description:"Vegitables for this week", created_date:"12-01-2020"},
+      {edit:false, id:2, title:"Shopping", description:"Monthly home goods", created_date:"14-01-2020"}
+    ]
+  }
+
+  //Delete todo
+  deleteTodo = (id) => {
+    const todos = this.state.todos.filter(todo => {
+      return todo.id !== id
+    });
+    this.setState({
+      todos //todos: todos - no need to use since key and value having the same name
+    });
+  }
+
+  //Add todo
+  addTodo = (todo) => {
+    todo.id = Math.random();
+    let todos = [...this.state.todos, todo];
+    this.setState({
+      todos
+    });
+  }
+
+  //Edit todo
+  editTodo = (todo) => {
+    this.setState({
+      edit:true
+    });
+  }
+
+  render() {
+    return (
+      <div className="todo-app container">
+        <h1 className="center blue-text">Todo's</h1>
+        <CreateTodo addTodo={this.addTodo}/>
+        <Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />
+      </div>
+    );
+  }
 }
 
 export default App;
